b2cc7b15718db5b55d8dfdb9af4d93f9b0e74b13
Viktoriya <57403423+ViGubareva@users.noreply.github.com>
2019-11-12 21:42:31 +0300

    Add files via upload

--- Массивы.cs
+++ Массивы.cs
@@ -7,18 +7,18 @@ namespace ArraySorting
     {
         static void Main ()
         {
-            Int[] arr = new Int[] {-3, 4, 666, 19, 100500, 42, -10, 0}
+            int[] arr = new int[] {-3, 4, 666, 19, 100500, 42, -10, 0}
             // посмотреть изначальное состояние
             Console.Write("Start ");
-            for (Int i = 0; i < arr.Length; i += 1)
+            for (int i = 0; i < arr.Length; i += 1)
             {
                 Console.Write(" {0}", arr[i]);
             }
-            for (Int i = 0; i < arr.Length - 1; i += 1)
+            for (int i = 0; i < arr.Length - 1; i += 1)
             {
                 // найти минимальное:
-                Int iMin = i;
-                for (Int k = i + 1; k < arr.Length; k += 1)
+                int iMin = i;
+                for (int k = i + 1; k < arr.Length; k += 1)
                 {
                     if (arr[k] < arr[iMin])
                     {
@@ -28,15 +28,16 @@ namespace ArraySorting
                 // поменять значения местами
                 if (iMin != i)
                 {
-                    Int f = arr[iMin];
+                    int f = arr[iMin];
                     arr[iMin] = arr[i];
                     arr[i] = f;
                 }
                 // вывод результата сортировки пошагово
                 Console.Write("\nstep {0} :", i);
-                for (Int j = 0; j < arr.Length; j += 1)
+                for (int j = 0; j < arr.Length; j += 1)
                 {
                     Console.Write(" {0}", arr[j]);
+                    Console.ReadKey();
                 }
             }
         }
